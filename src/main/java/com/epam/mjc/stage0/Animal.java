f7a2c7283e684bb0268ab8d241db8c9bad2a6838
Igor Chvalun <igor.chvalun@investengine.com>
2023-09-28 14:22:57 +0530

    The last one here

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        // it is a constructor, which is a special method
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String mostly = "This animal is mostly " + color + ". It has " + numberOfPaws + " " + (numberOfPaws > 1 ? "paws" : "paw") + " and " + ((hasFur) ? "a" : "no") + " fur.";
+        return mostly;
+    }
 
 }
